71cc43f8dc0e59114e701f0db3a713fd233835f3
gigamon <gigamon@hotmail.com>
2020-10-26 12:03:00 -0400

    Changes to LittleEndianBitConverter, but slowly switching over to using BinaryPrimitives instead (e.g. already switched DataLocation). - Removed 8-bit methods since they were useless. - Added methods for Span<byte>.

--- src/Utilities/LittleEndianBitConverter.cs
+++ src/Utilities/LittleEndianBitConverter.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace SS.Utilities
 {
@@ -10,33 +7,18 @@ namespace SS.Utilities
     /// </summary>
     public static class LittleEndianBitConverter
     {
-        #region Read 8-bit
-
-        public static sbyte ToSByte(byte[] source, int byteOffset)
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-
-            if (byteOffset < 0 || byteOffset >= source.Length)
-                throw new ArgumentException($"{nameof(byteOffset)} is not a valid position to read from {nameof(source)}.");
-
-            return (sbyte)source[byteOffset];
-        }
-
-        public static byte ToByte(byte[] source, int byteOffset)
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-
-            if (byteOffset < 0 || byteOffset >= source.Length)
-                throw new ArgumentException($"{nameof(byteOffset)} is not a valid position to read from {nameof(source)}.");
-
-            return source[byteOffset];
-        }
-
-        #endregion
-
         #region Read 16-bit
+
+        public static short ToInt16(Span<byte> source)
+        {
+            if (source.Length < 2)
+                throw new ArgumentException("Length must be at least 2 bytes.", nameof(source));
+
+            if (UseBitConverter)
+                return BitConverter.ToInt16(source);
+            else
+                return (short)(source[0] | (source[1] << 8));
+        }
 
         public static short ToInt16(byte[] source, int byteOffset)
         {
@@ -50,6 +32,17 @@ namespace SS.Utilities
                 return BitConverter.ToInt16(source, byteOffset);
             else
                 return (short)(source[byteOffset] | (source[byteOffset + 1] << 8));
+        }
+
+        public static ushort ToUInt16(Span<byte> source)
+        {
+            if (source.Length < 2)
+                throw new ArgumentException("Length must be at least 2 bytes.", nameof(source));
+
+            if (UseBitConverter)
+                return BitConverter.ToUInt16(source);
+            else
+                return (ushort)(source[0] | (source[1] << 8));
         }
 
         public static ushort ToUInt16(byte[] source, int byteOffset)
@@ -70,6 +63,20 @@ namespace SS.Utilities
 
         #region Read 32-bit
 
+        public static int ToInt32(Span<byte> source)
+        {
+            if (source.Length < 4)
+                throw new ArgumentException("Length must be at least 4 bytes.", nameof(source));
+
+            if (UseBitConverter)
+                return BitConverter.ToInt32(source);
+            else
+                return source[0]
+                    | (source[1] << 8)
+                    | (source[2] << 16)
+                    | (source[3] << 24);
+        }
+
         public static int ToInt32(byte[] source, int byteOffset)
         {
             if (source == null)
@@ -85,6 +92,20 @@ namespace SS.Utilities
                     | (source[byteOffset + 1] << 8)
                     | (source[byteOffset + 2] << 16)
                     | (source[byteOffset + 3] << 24);
+        }
+
+        public static uint ToUInt32(Span<byte> source)
+        {
+            if (source.Length < 4)
+                throw new ArgumentException("Length must be at least 4 bytes.", nameof(source));
+
+            if (UseBitConverter)
+                return BitConverter.ToUInt32(source);
+            else
+                return (uint)(source[0]
+                    | (source[1] << 8)
+                    | (source[2] << 16)
+                    | (source[3] << 24));
         }
 
         public static uint ToUInt32(byte[] source, int byteOffset)
@@ -108,6 +129,24 @@ namespace SS.Utilities
 
         #region Read 64-bit
 
+        public static long ToInt64(Span<byte> source)
+        {
+            if (source.Length < 8)
+                throw new ArgumentException("Length must be at least 8 bytes.", nameof(source));
+
+            if (UseBitConverter)
+                return BitConverter.ToInt64(source);
+            else
+                return (long)source[0]
+                    | ((long)source[1] << 8)
+                    | ((long)source[2] << 16)
+                    | ((long)source[3] << 24)
+                    | ((long)source[4] << 32)
+                    | ((long)source[5] << 40)
+                    | ((long)source[6] << 48)
+                    | ((long)source[7] << 56);
+        }
+
         public static long ToInt64(byte[] source, int byteOffset)
         {
             if (source == null)
@@ -129,6 +168,24 @@ namespace SS.Utilities
                     | ((long)source[byteOffset + 7] << 56);
         }
 
+        public static ulong ToUInt64(Span<byte> source)
+        {
+            if (source.Length < 8)
+                throw new ArgumentException("Length must be at least 8 bytes.", nameof(source));
+
+            if (UseBitConverter)
+                return BitConverter.ToUInt64(source);
+            else
+                return (ulong)source[0]
+                    | ((ulong)source[1] << 8)
+                    | ((ulong)source[2] << 16)
+                    | ((ulong)source[3] << 24)
+                    | ((ulong)source[4] << 32)
+                    | ((ulong)source[5] << 40)
+                    | ((ulong)source[6] << 48)
+                    | ((ulong)source[7] << 56);
+        }
+
         public static ulong ToUInt64(byte[] source, int byteOffset)
         {
             if (source == null)
@@ -148,52 +205,6 @@ namespace SS.Utilities
                     | ((ulong)source[byteOffset + 5] << 40)
                     | ((ulong)source[byteOffset + 6] << 48)
                     | ((ulong)source[byteOffset + 7] << 56));
-        }
-
-        #endregion
-
-        #region Write 8-bit
-
-        public static bool TryWriteBytes(Span<byte> dest, byte val)
-        {
-            if (dest == null || dest.IsEmpty || dest.Length < 1)
-                return false;
-
-            dest[0] = val;
-            return true;
-        }
-
-        public static bool TryWriteBytes(byte[] dest, int byteOffset, byte val)
-        {
-            if (dest == null)
-                return false;
-
-            if (byteOffset < 0 || byteOffset >= dest.Length)
-                return false;
-
-            dest[byteOffset] = val;
-            return true;
-        }
-
-        public static bool TryWriteBytes(Span<byte> dest, sbyte val)
-        {
-            if (dest == null || dest.IsEmpty || dest.Length < 1)
-                return false;
-
-            dest[0] = (byte)val;
-            return true;
-        }
-
-        public static bool TryWriteBytes(byte[] dest, int byteOffset, sbyte val)
-        {
-            if (dest == null)
-                return false;
-
-            if (byteOffset < 0 || byteOffset >= dest.Length)
-                return false;
-
-            dest[byteOffset] = (byte)val;
-            return true;
         }
 
         #endregion
