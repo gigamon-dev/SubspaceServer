5c85cc240d0e9025450d60b562fc663a57ed48eb
gigamon <gigamon@hotmail.com>
2026-03-28 19:06:49 -0400

    Add missing PlayerGroupMemberAddedCallback.

---
+++ src/Matchmaking/Callbacks/PlayerGroupMemberAddedCallback.cs
@@ -0,0 +1,18 @@
+﻿using SS.Core;
+
+namespace SS.Matchmaking.Callbacks
+{
+    /// <summary>
+    /// Callback for when a member has been added to a <see cref="IPlayerGroup"/>.
+    /// </summary>
+    [CallbackHelper]
+    public static partial class PlayerGroupMemberAddedCallback
+    {
+        /// <summary>
+        /// Delegate for when a member has been added to a <see cref="IPlayerGroup"/>
+        /// </summary>
+        /// <param name="group">The group that the member was added to.</param>
+        /// <param name="player">The player added to the group.</param>
+        public delegate void PlayerGroupMemberAddedDelegate(IPlayerGroup group, Player player);
+    }
+}
